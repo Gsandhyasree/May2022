744bfed91f937f94d32baaef004449cc820ed73c
Sandhya Sree <sandhyasree@rameshs-air.lan>
2022-05-30 23:45:26 +0800

    my first checkin

---
+++ May2022/May2022/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace May2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //open chrome Browser
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Manage().Window.Maximize();
+
+
+
+            //launch turn up portal
+
+            driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+
+            //identify username textbox and enter valid user name
+
+            IWebElement usertextbox = driver.FindElement(By.Id("UserName"));
+
+            usertextbox.SendKeys("hari");
+
+            //identify password textbox and enter valid password
+
+            IWebElement passwordtextbox = driver.FindElement(By.Id("Password"));
+
+            passwordtextbox.SendKeys("123123");
+
+
+            //identify login button and click login button
+            IWebElement loginbutton = driver.FindElement(By.XPath("//*[@id='loginForm']/form/div[3]/input[1]"));
+
+            loginbutton.Click();
+
+            //checked if user has logged in successfully
+
+
+            IWebElement hellohari = driver.FindElement(By.XPath("//*[@id='logoutForm']/ul/li/a"));
+
+
+           if(hellohari.Text == "Hello hari!"){
+
+                Console.WriteLine("login successfull");
+            }
+
+            else
+            {
+                Console.WriteLine("login failed");
+
+            }
+
+            //Thread.Sleep(30000);
+
+        }
+    }
+}
